219613b1a4ec5f042a8f53d9c3a5f3aaa8548c01
Michael Krumdick <mrkrumdick@gmail.com>
2022-04-18 15:06:58 +0000

    Making model size an optimization parameter

--- run.sh
+++ run.sh
@@ -1,8 +1,10 @@
 #! /bin/bash
 
+NL=$1
+
 while true; do
-    python main.py config.yml $(mktemp -p /nfs/das_data -d) --DFA.n_states 4 --model.nlayers 2
-    python main.py config.yml $(mktemp -p /nfs/das_data -d) --DFA.n_states 6 --model.nlayers 2
-    python main.py config.yml $(mktemp -p /nfs/das_data -d) --DFA.n_states 8 --model.nlayers 2
-    python main.py config.yml $(mktemp -p /nfs/das_data -d) --DFA.n_states 10 --model.nlayers 2
+    python main.py config.yml $(mktemp -p /nfs/das_data -d) --DFA.n_states 4 --model.nlayers $NL
+    python main.py config.yml $(mktemp -p /nfs/das_data -d) --DFA.n_states 6 --model.nlayers $NL
+    python main.py config.yml $(mktemp -p /nfs/das_data -d) --DFA.n_states 8 --model.nlayers $NL
+    python main.py config.yml $(mktemp -p /nfs/das_data -d) --DFA.n_states 10 --model.nlayers $NL
 done
